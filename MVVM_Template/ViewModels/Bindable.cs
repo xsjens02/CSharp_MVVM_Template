956e3dfbe101bceaeae3d05bca6d2053e247e39c
xsjens02 <simjen02@easv365.dk>
2024-10-04 20:58:26 +0200

    Add project files.

---
+++ MVVM_Template/ViewModels/Bindable.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MVVM_Template.ViewModels
+{
+    public class Bindable : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] String name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
---
+++ MVVM_Template/ViewModels/RelayCommand.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows.Input;
+
+namespace MVVM_Template.ViewModels
+{
+    public class RelayCommand : ICommand
+    {
+        private Action _execute;
+        private Func<bool> _canExecute;
+
+        public RelayCommand(Action execute, Func<bool> canExecute)
+        {
+            this._execute = execute;
+            this._canExecute = canExecute;
+        }
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return _canExecute();
+        }
+
+        public void Execute(object? parameter)
+        {
+            _execute();
+        }
+    }
+}
